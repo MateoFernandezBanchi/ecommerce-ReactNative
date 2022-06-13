480c1b5b8d18e8d6c3d356362334755c6394b9bc
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-06-13 00:52:28 -0300

    camaraAdvance

--- Screens/SaveLocationScreen.js
+++ Screens/SaveLocationScreen.js
@@ -1,17 +1,47 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ImagePickerIOS, Image } from 'react-native';
 import React from 'react';
 import {useDispatch} from 'react-redux';
 import { addLocation } from '../Features/Locations';
+import * as ImagePicker from 'expo-image-picker';
 
 const SaveLocationScreen = () => {
     const [title, setTitle] = React.useState("");
     const [picture, setPicture] = React.useState("");
     const dispatch = useDispatch();
-    const handleTakePicture = () => {
 
+    const getPermission = async () => {
+        const {status} = await ImagePicker.getMediaLibraryPermissionsAsync()
+        console.log (status);
+        if (status !== "granted") {
+            return false;
+    } return true;
+}
+const handleTakePicture = async () => {
+    const isVerified = getPermission()
+    if (!isVerified) {
+        return;
     }
-    const handlePickLibrary = () => {
+    const image = await ImagePicker.launchCameraAsync ({
+        allowsEditing: true,
+        aspect: [4,3],
+        quality: 1,
+    })
+    setPicture(image.uri);
+}
 
+    const handlePickLibrary = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync ({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            alowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+
+        });
+        console.log(result);
+
+        if (!result.cancelled) {
+            setPicture(result.uri);
+        }
     }
     const handleConfirm = () => {
         dispatch(addLocation(title));
